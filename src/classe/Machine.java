6e00dc9b508b1c0054a4defad09d42eeb03e258a
imadeddine <imadeddine@192.168.1.7>
2018-11-10 00:58:16 +0100

    GestionMachineProjet

---
+++ src/classe/Machine.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classe;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author imadeddine
+ */
+@Entity
+public class Machine {
+     @Id
+    @GeneratedValue
+    private int id;
+    private String marque;
+    private String reference;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateAchat;
+    private double prix;
+
+    public Machine() {
+    }
+
+    public Machine(String marque, String reference, Date dateAchat, double prix) {
+        this.marque = marque;
+        this.reference = reference;
+        this.dateAchat = dateAchat;
+        this.prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Machine(int id, String marque, String reference, Date dateAchat, double prix) {
+        this.id = id;
+        this.marque = marque;
+        this.reference = reference;
+        this.dateAchat = dateAchat;
+        this.prix = prix;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+    
+    
+    
+    
+}
---
+++ src/service/MachineService.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import IDao.IDao;
+import classe.Machine;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+/**
+ *
+ * @author imadeddine
+ */
+public class MachineService implements IDao<Machine> {
+
+    @Override
+    public boolean create(Machine o) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(o);
+            tx.commit();
+            session.close();
+            return true;
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            session.close();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Machine o) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.update(o);
+            tx.commit();
+            session.close();
+            return true;
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            session.close();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(Machine o) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(o);
+            tx.commit();
+            session.close();
+            return true;
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            session.close();
+            return false;
+        }
+
+    }
+
+    @Override
+    public List<Machine> findAll() {
+        List<Machine> machines = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            machines = session.createQuery("from Machine").list();
+            tx.commit();
+            session.close();
+            return machines;
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            session.close();
+            return machines;
+        }
+
+    }
+
+    @Override
+    public Machine findById(int id) {
+        Machine m = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            m = (Machine) session.get(Machine.class, id);
+            tx.commit();
+            session.close();
+            return m;
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            session.close();
+            return m;
+        }
+
+    }
+
+}
